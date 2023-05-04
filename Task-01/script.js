71d18c78c57b915cb42bf6ec26461b3e377cd291
abhikrishnaram <abhikrishnaram88@gmail.com>
2023-05-04 09:12:51 +0530

    Initial commit

--- Task-01/script.js
+++ Task-01/script.js
@@ -1,5 +1,27 @@
 const root = document.querySelector('html');
 
+const DATA = [
+  {
+    description: 'Team',
+    name: 'Avengers',
+  },
+  {
+    description: 'Member 1',
+    name: 'Vishu Datta',
+  },
+  {
+    description: 'Member 2',
+    name: 'Sidharth A V',
+  },
+  {
+    description: 'Member 3',
+    name: 'Varnika Sood'
+  },
+  {
+    description: 'Member 4',
+    name: 'Abhiram Krishna'
+  }
+]
 
 const cursor = document.createElement('div');
 cursor.classList.add('cursor');
@@ -10,6 +32,23 @@ const follower = document.createElement('div');
 follower.classList.add('cursor', 'cursor__follower');
 root.appendChild(follower);
 
+let current = 0;
+
+function showPrev(){
+  if(current > 0)
+    current--;
+  
+   document.getElementById('mem_name').innerText = DATA[current].name;
+   document.getElementById('mem_title').innerText = DATA[current].description;
+};
+
+function showNext(){
+  if(current<4)
+    current++;
+  
+   document.getElementById('mem_name').innerText = DATA[current].name; 
+   document.getElementById('mem_title').innerText = DATA[current].description; 
+};
 
 root.addEventListener('mousemove', e => {
   setPosition(follower, e);
